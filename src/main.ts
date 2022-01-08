ed8e601454fc74967413737d2ac7dc7202b9ac20
undnull <41549137+undnull@users.noreply.github.com>
2022-01-08 21:10:07 +0500

    Stuff

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log("Hello, wonderful world!");
+import * as fs from "fs";
+
+class UnixTime {
+    millis: number;
+
+    constructor(value?: Date | number | undefined)
+    {
+        switch(typeof value) {
+            case "object":
+                this.millis = value.getTime();
+                break;
+            case "number":
+                this.millis = value;
+                break;
+            default:
+                this.millis = (new Date()).getTime();
+                break;
+        }
+    }
+
+    write(path: string) {
+        console.log("writing", this.millis, "->", path);
+        fs.writeFileSync(path, this.millis.toString());
+    }
+};
+
+const ft: UnixTime = new UnixTime(0);
+const ct: UnixTime = new UnixTime();
+
+ft.write("ft.txt");
+ct.write("ct.txt");
